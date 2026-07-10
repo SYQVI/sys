dbbb3f15813bb15fd6edd1530ed50f6272abcdb6
SYQVI <waeililnzy@gmail.com>
2026-07-10 18:53:44 +0300

    Update punishment description in Arabic

--- index.js
+++ index.js
@@ -123,7 +123,7 @@ client.on(Events.InteractionCreate, async (interaction) => {
 
         PUNISHMENT_REASONS[type].push({
             label: displayLabel,
-            description: `عقوبة مخصصة مضافة بواسطة الإدارة العليا`,
+            description: `عقوبة مضافة بواسطة الإدارة العليا`,
             value: uniqueValue
         });
 
